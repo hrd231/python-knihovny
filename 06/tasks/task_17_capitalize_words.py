e33bd0ef5efc339f4fcdbc8061bb6049bdef8485
Pavel Hrdinka <pavel.hrdinka@mavenir.com>
2025-03-31 19:02:05 +0200

    :task_17_capitalize_words.py

--- 06/tasks/task_17_capitalize_words.py
+++ 06/tasks/task_17_capitalize_words.py
@@ -4,4 +4,7 @@ def capitalize_words(s: str) -> str:
     """
     Vrátí řetězec, kde každé slovo začíná velkým písmenem.
     """
+    
+    return s.title()
 
+print(capitalize_words("tento text upravime a vsechny pismena budou velke"))
