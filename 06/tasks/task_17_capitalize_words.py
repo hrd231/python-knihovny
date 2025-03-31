4b83dfd94a57dc7f763b80bb6de28a7e17d811f2
Pavel Hrdinka <phrdinka.eprin@gmail.com>
2025-03-31 19:46:05 +0200

    uprava dle autopep8

--- 06/tasks/task_17_capitalize_words.py
+++ 06/tasks/task_17_capitalize_words.py
@@ -1,10 +1,10 @@
 # task_17_capitalize_words.py
 
+
 def capitalize_words(s: str) -> str:
     """
     Vrátí řetězec, kde každé slovo začíná velkým písmenem.
     """
-    
-    return s.title()
 
-print(capitalize_words("tento text upravime a vsechny pismena budou velke"))
+    return s.title()
+    
